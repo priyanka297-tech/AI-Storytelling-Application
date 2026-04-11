47630b96dcae0bd4b8454eeeedaf7f4ce3fe1024
Priyanka PAL <priyankapal29702@gmail.com>
2026-04-11 15:05:16 +0530

    Initial clean commit

---
+++ own_agents.py
@@ -0,0 +1,23 @@
+from groq import Groq
+
+client = Groq(
+  api_key="GROQ_API_KEY"
+)
+
+
+def generate_story(prompt, genre, length):
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "system",
+                "content": f"Write a {length.lower()} {genre.lower()} story."
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+    return response.choices[0].message.content
+
